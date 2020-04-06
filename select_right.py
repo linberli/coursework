48a02b16c5071b4a459d8676d26bdbf1d3c20431
linberli <55867860+linberli@users.noreply.github.com>
2020-04-06 20:52:06 +0500

    Update select_right.py

--- select_right.py
+++ select_right.py
@@ -1,18 +1,23 @@
+def select(arr, dim):
+    k = 0
+    alg_count = [0, 0]
+    for k in range(0, dim - 1):
+        m = k
+        i = k + 1
+        for i in range(i, dim):
+            alg_count[0] += 1
+            if arr[i] < arr[m]:
+                m = i
+            i += 1
+        if k != m:
+            t = arr[k]
+            arr[k] = arr[m]
+            arr[m] = t
+            alg_count[1] += 1
+        k += 1
+    print(alg_count)
 import random
-def select (arr, dim):
-    alg_count=[0,0]
-    for i in range(0,dim-1):
-        first= arr[i]
-        minindex=i
-        for j in range(i+1,dim):
-            alg_count[0]+=1
-            if arr[minindex]>arr[j]:
-                minindex=j
-                alg_count[1]+=1
-        arr[i],arr[minindex]=arr[minindex],arr[i]
-    return alg_count
-a= [1,2,3,4,5]
-random.shuffle(a)
-b = 5
-select(a,b)
-
+arry = [random.randint(0, 100000) for i in range(100000)]
+print(arry)
+select(arry, len(arry))
+print(arry)

fe72751f04c373d879b70fae132c5245f85269c0
linberli <55867860+linberli@users.noreply.github.com>
2020-04-06 21:03:54 +0500

    Update bubble.py

--- bubble.py
+++ bubble.py
@@ -1,15 +1,14 @@
 def bubble(arr, dim):
     alg_count = [0, 0]
-    for i in range(0, dim-1):
-        j = dim-1
-       while j>i:
-            alg_count[0]+=1
-            if arr[j]<arr[j-1]:
-                alg_count[1]+=1
-                arr[j],arr[j-1]=arr[j-1],arr[j]
-            j-=1
-    return alg_count
-    
-a = [5,4,3,2,1]
-b = 5
-bubble(a,b)
+    for i in range(dim - 1):
+        alg_count[0] += 1
+        for j in range(dim - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                alg_count[1] += 1
+    print(alg_count)
+import random
+arry = [random.randint(0, 100000) for i in range(100000)]
+print(arry)
+bubble(arry, len(arry))
+print(arry)
